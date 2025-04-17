aca1cfaf8b44a67963b90ddfd741a532f6a03b45
Ajeeth <ajeetheswari14@gmail.com>
2025-04-17 10:59:28 +0530

    mobile

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,34 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
+import { VitePWA } from "vite-plugin-pwa";
+
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "abi.png"],
+      manifest: {
+        name: "NextDevPath",
+        short_name: "NextDev",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#000000",
+        theme_color: "#1d4ed8",
+        icons: [
+          {
+            src: "ajeeth.JPG",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "ajeeth.JPG",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
